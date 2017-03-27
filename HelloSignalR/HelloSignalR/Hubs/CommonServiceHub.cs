78caf99ad75687ba959e54f6f91c8f4de0710ddf
Damien Sawyer <damiensawyer@gmail.com>
2017-03-27 23:10:34 +1100

    service

---
+++ HelloSignalR/HelloSignalR/Hubs/CommonServiceHub.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelloSignalR.Services;
+using Microsoft.AspNet.SignalR;
+
+namespace HelloSignalR.Hubs
+{
+    public class CommonServiceHub : Hub
+    {
+        private readonly SampleService _sampleService;
+
+        public CommonServiceHub() : this(SampleService.Instance) // this will kick off the service which is Lazy
+        {
+        }
+
+        public CommonServiceHub(SampleService sampleService)
+        {
+            this._sampleService = sampleService;
+        }
+        public void StopTimer()
+        {
+            this._sampleService.StopTimer();
+        }
+
+        public void RestartTimer()
+        {
+            this._sampleService.RestartTimer();
+        }
+    }
+}
