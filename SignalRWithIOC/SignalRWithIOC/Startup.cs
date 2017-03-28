ac964688fe177ece99152dd36e95566ba18e32f7
Damien Sawyer <damiensawyer@gmail.com>
2017-03-28 20:09:29 +1100

    tried all in global

--- SignalRWithIOC/SignalRWithIOC/Startup.cs
+++ SignalRWithIOC/SignalRWithIOC/Startup.cs
@@ -25,55 +25,7 @@ namespace HelloSignalR
     {
         public void Configuration(IAppBuilder app)
         {
-            //didn't get working
-            // try this next? http://stackoverflow.com/a/29793864/494635
-
-            var builder = new ContainerBuilder();
-            builder.RegisterType<LoggingPipelineModule>(); // see http://docs.autofac.org/en/latest/integration/owin.html for registering custom pipeline modules
-            var config = new HubConfiguration();
-
-            // Services common with SignalR and MVC
-            //RegisterServices.RegisterCommonServices(builder);
-
-            // Register SignalR Specific.
-            builder.RegisterHubs(Assembly.GetExecutingAssembly());
-
-
-
-
-            //        builder.Register(ctx =>
-            //ctx.Resolve<IDependencyResolver>()
-            //   .Resolve<IConnectionManager>()
-            //   .GetHubContext<EventHub>())
-            //   .Named<IHubContext>("EventHub");
-
-            //builder.RegisterInstance(GlobalHost.ConnectionManager).As<IConnectionManager>()
-
-            // Attempts to get HubContexts into services... so that they can send to clients. 
-            // http://stackoverflow.com/a/37913821
-            builder.Register((ctx, p) =>
-            config.Resolver.Resolve<IConnectionManager>() // this works... but wtf. Why can't I use ctx? 
-                    .GetHubContext<IOCHub>())
-                .Named<IHubContext>("IOCHub");
-
-            builder.RegisterType<NameService>().WithParameter(
-                new ResolvedParameter(
-                    (pi, ctx) => pi.ParameterType == typeof(IHubContext),
-                    (pi, ctx) => ctx.ResolveNamed<IHubContext>("IOCHub")
-                )
-            );
-
-            var container = builder.Build();
-            config.Resolver = new AutofacDependencyResolver(container); // note, this is a different AutofacDependencyResolver to the one in global.asax. Different namespace
-
-            app.UseAutofacMiddleware(container);
-            app.MapSignalR("/signalr", config);
-
-            //// To add custom HubPipeline modules, you have to get the HubPipeline
-            //// from the dependency resolver, for example:
-
-            var hubPipeline = config.Resolver.Resolve<IHubPipeline>();
-            hubPipeline.AddModule(config.Resolver.Resolve<LoggingPipelineModule>());
+            app.MapSignalR();
 
         }
     }
